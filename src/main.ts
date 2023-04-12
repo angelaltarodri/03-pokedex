7498c8e655a0f76aa2b328ab38b8495ac834fcd8
Angel Altamirano <angelaltarodri@gmail.com>
2023-04-12 13:19:57 -0500

    first

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // white list solo deja que la data necesaria sea ingresada, SOLO la necesaria
+      whitelist: true,
+      // "property banana should not exist"
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.setGlobalPrefix('api/v2');
+  await app.listen(3000);
+}
+bootstrap();
